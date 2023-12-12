b8730cabb938895a5c003671f2605df4f2e35b7d
SamuelReginaldo <usr1@gmail.com>
2023-12-12 15:11:52 +0100

    Creacion menu

--- Tanks_II/Assets/Scripts/ScriptsMenu/Script_Menu.cs
+++ Tanks_II/Assets/Scripts/ScriptsMenu/Script_Menu.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Script_Menu : MonoBehaviour
 {
+    public AudioMixer mixer;
+    public Slider volumenMaster;
+    public Slider volumenFX;
+    public Toggle silenciar;
+
+    private void Awake()
+    {
+        volumenMaster.onValueChanged.AddListener(cambiarVolumenMaster);
+        volumenFX.onValueChanged.AddListener(cambiarVolumenFX);
+    }
     public void EmpezarNivel(string NombreNivel)
     {
         SceneManager.LoadScene(NombreNivel);
@@ -13,5 +26,14 @@ public class Script_Menu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Aquí se cierra el juego");
+    }
+
+    public void cambiarVolumenMaster(float v)
+    {
+        mixer.SetFloat("volMaster", v);
+    }
+    public void cambiarVolumenFX(float v)
+    {
+        mixer.SetFloat("volMusica", v);
     }
 }
